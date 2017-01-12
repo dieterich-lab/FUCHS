64bdd72c2e09be2a81f3a15e33a2f1ff76471d0f
Tobias Jakobi <tobias.jakobi@med.uni-heidelberg.de>
2017-01-12 15:42:24 +0100

    Removed all textual R output

--- scripts/summarized_coverage_profiles.R
+++ scripts/summarized_coverage_profiles.R
@@ -1,7 +1,7 @@
 #! /usr/bin/env Rscript
 
 # let's make sure all packages we need are actually installed
-if (!require("pacman")) install.packages("pacman", repos = "http://cran.us.r-project.org")
+if (!require("pacman",quietly=TRUE)) install.packages("pacman", repos = "http://cran.us.r-project.org")
 pacman::p_load(amap, Hmisc, gplots)
 
 # script to assign relative position to coverage profiles of circles
@@ -68,7 +68,7 @@ if(length(summary_table_long_circle[,1]) > 0){
 } else {
   print('ERROR no long circle in the table')
 }
-dev.off()
+invisible(dev.off())
 
 
 # write cluster table -> check for strandedness of hostgene maybe the dip-shift correlates with strand.
@@ -92,7 +92,7 @@ choose_centers <- function(D){
 
 # cluster all circles 
 centers = choose_centers(summary_table)
-print(centers)
+#print(centers)
 if(centers > 1){
   kk = Kmeans(summary_table[,-c(1,2)], centers = centers, method = 'correlation', iter.max = 50, nstart = 100)
   
@@ -102,7 +102,7 @@ if(centers > 1){
 	plot(kk[[2]][i,], main = paste('Cluster Size: ', kk$size[i], sep = '') , type = 'l', lwd = 5, col = 'dodgerblue', ylab = 'avg. coverage', xlab = 'relative position', cex.axis = 1.7, cex.lab = 1.7, cex.main = 2 )
       }
     }
-  dev.off()
+  invisible(dev.off())
   circle_to_cluster_association <- data.frame(circle_id = summary_table[,1], length = summary_table[,2], cluster_id = kk$cluster)
   write.table(circle_to_cluster_association, paste(folder,'cluster_association.all_circles.tsv', sep = '/'), sep = '\t', row.names = F, quote = F)
   write.table(kk[[2]], paste(folder,'cluster_means.all_circles.tsv', sep = '/'), sep = '\t', quote = F, row.names = T)
@@ -113,7 +113,7 @@ if(centers > 1){
 
 # cluster short circles
 centers = choose_centers(summary_table_short_circle)
-print(centers)
+#print(centers)
 if(centers > 1){
   kk.short = Kmeans(summary_table_short_circle[,-c(1,2)], centers = centers, method = 'correlation', iter.max = 50, nstart = 50)
   
@@ -123,7 +123,7 @@ if(centers > 1){
 	plot(kk.short[[2]][i,], main = paste('Cluster Size: ', kk.short$size[i], sep = '') , type = 'l', lwd = 5, col = 'dodgerblue', ylab = 'avg. coverage', xlab = 'relative position', cex.axis = 1.7, cex.lab = 1.7, cex.main = 2 )
       }
     }
-  dev.off()
+  invisible(dev.off())
   circle_to_cluster_association.short <- data.frame(circle_id = summary_table_short_circle[,1], length = summary_table_short_circle[,2], cluster_id = kk.short$cluster)
   write.table(circle_to_cluster_association.short, paste(folder,'cluster_association.short_circles.tsv', sep = '/'), sep = '\t', row.names = F, quote = F)
   write.table(kk.short[[2]], paste(folder,'cluster_means.short_circles.tsv', sep = '/'), sep = '\t', quote = F, row.names = T)
@@ -142,7 +142,7 @@ if(centers > 1){
 	plot(kk.medium[[2]][i,], main = paste('Cluster Size: ', kk.medium$size[i], sep = '') , type = 'l', lwd = 5, col = 'dodgerblue', ylab = 'avg. coverage', xlab = 'relative position', cex.axis = 1.7, cex.lab = 1.7, cex.main = 2 )
       }
     }
-  dev.off()
+  invisible(dev.off())
 circle_to_cluster_association.medium <- data.frame(circle_id = summary_table_medium_circle[,1], length = summary_table_medium_circle[,2], cluster_id = kk.medium$cluster)
 write.table(circle_to_cluster_association.medium, paste(folder,'cluster_association.medium_circles.tsv', sep = '/'), sep = '\t', row.names = F, quote = F)
 write.table(kk.medium[[2]], paste(folder,'cluster_means.medium_circles.tsv', sep = '/'), sep = '\t', quote = F, row.names = T)
@@ -154,18 +154,18 @@ write.table(kk.medium[[2]], paste(folder,'cluster_means.medium_circles.tsv', sep
 
 # cluster long circles
 centers = choose_centers(summary_table_long_circle)
-print(centers)
+#print(centers)
 if(centers > 1){
   kk.long = Kmeans(summary_table_long_circle[,-c(1,2)], centers = centers, method = 'correlation', iter.max = 50, nstart = 100)
   
   pdf(paste(folder,'coverage.clusters.long_circles.pdf', sep = '/'))
     for(i in 1:centers) {
-      	print(kk.long$size[i])
+#      	print(kk.long$size[i])
 	if(kk.long$size[i] > 0){
 	plot(kk.long[[2]][i,], main = paste('Cluster Size: ', kk.long$size[i], sep = '') , type = 'l', lwd = 5, col = 'dodgerblue', ylab = 'avg. coverage', xlab = 'relative position', cex.axis = 1.7, cex.lab = 1.7, cex.main = 2 )
       }
     }
-  dev.off()
+invisible(dev.off())
   circle_to_cluster_association.long <- data.frame(circle_id = summary_table_long_circle[,1], length = summary_table_long_circle[,2], cluster_id = kk.long$cluster)
   write.table(circle_to_cluster_association.long, paste(folder,'cluster_association.long_circles.tsv', sep = '/'), sep = '\t', row.names = F, quote = F)
   write.table(kk.long[[2]], paste(folder,'cluster_means.long_circles.tsv', sep = '/'), sep = '\t', quote = F, row.names = T)
@@ -173,4 +173,4 @@ if(centers > 1){
   print('ERROR not enough long circles in table to perform a clustering')
 }
 
-warnings()
+#warnings()
