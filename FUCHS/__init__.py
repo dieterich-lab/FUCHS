ae6adf8241e15e34779c141823a5e48054b98789
Tobias Jakobi <tobias.jakobi@med.uni-heidelberg.de>
2017-09-26 17:09:13 +0200

    Reformatted indents

--- FUCHS/__init__.py
+++
@@ -1,8 +0,0 @@
-# Import modules
-import get_readnames_from_DCC as get_readnames
-import extract_reads as extract_reads
-import get_mate_information as mateinformation
-import detect_skipped_exons as skipped_exons
-import detect_splicing_variants as splicing_variants
-import get_coverage_profile as coverage_profile
-from FUCHS import main
