eb525744c769daec690506b979d8b55744534965
Tobias Jakobi <tobias.jakobi@med.uni-heidelberg.de>
2017-09-28 11:34:21 +0200

    Fixing indents

--- FUCHS/get_readnames_from_DCC.py
+++ FUCHS/get_readnames_from_DCC.py
@@ -5,7 +5,7 @@
 # of mate1/2 to extract read names of circle spanning reads
 
 class get_readnames_from_DCC(object):
-    def __init__(self,circlefile, junction_file, mate1, mate2,):
+    def __init__(self, circlefile, junction_file, mate1, mate2, ):
 
         self.circle_file = circlefile
         self.junction_file = junction_file
@@ -17,9 +17,9 @@ class get_readnames_from_DCC(object):
         input_file = open(infile)
         circIDs = []
         for line in input_file:
-	    if not line.startswith('#') and not line.startswith('Chr\t'):
-		current_line = line.replace('\n', '').split('\t')
-		circIDs += [(current_line[0], int(current_line[1]), int(current_line[2]))]
+            if not line.startswith('#') and not line.startswith('Chr\t'):
+                current_line = line.replace('\n', '').split('\t')
+                circIDs += [(current_line[0], int(current_line[1]), int(current_line[2]))]
         input_file.close()
         return circIDs
 
@@ -32,14 +32,14 @@ class get_readnames_from_DCC(object):
             start = min(coord)
             end = max(coord)
             if not (chrom, start + 1, end - 1) in reads:
-                reads[(chrom, start + 1, end - 1)] = {'paired': [], 'mate1': [], 'mate2':[]}
+                reads[(chrom, start + 1, end - 1)] = {'paired': [], 'mate1': [], 'mate2': []}
             else:
                 reads[(chrom, start + 1, end - 1)]['paired'] += [current_line[9]]
         input_file.close()
         return reads
-    
+
     def read_mate_junction_file(self, infile, reads, mate):
-	input_file = open(infile)
+        input_file = open(infile)
         for line in input_file:
             current_line = line.replace('\n', '').split('\t')
             chrom = current_line[0]
@@ -47,11 +47,11 @@ class get_readnames_from_DCC(object):
             start = min(coord)
             end = max(coord)
             if not (chrom, start + 1, end - 1) in reads:
-                reads[(chrom, start + 1, end - 1)] = {'paired': [], 'mate1':[], 'mate2': []}
+                reads[(chrom, start + 1, end - 1)] = {'paired': [], 'mate1': [], 'mate2': []}
             else:
                 reads[(chrom, start + 1, end - 1)][mate] += [current_line[9]]
-	input_file.close()
-	return reads
+        input_file.close()
+        return reads
 
     def filter_circles_by_circID(self, circIDs, reads):
         sorted_keys = sorted(reads.keys())
@@ -59,21 +59,21 @@ class get_readnames_from_DCC(object):
             if not key in circIDs:
                 del reads[key]
         return reads
-    
+
     def filter_reads_by_mate(self, reads, is_paired):
-	unique_reads = {}
-	for circ in reads.keys():
-	    unique_reads[circ] = []
-	    all_reads = list(set(reduce(lambda x, y: x + y, reads[circ].values(), [])))
-	    if is_paired:
-		for read in all_reads:
-		    if read in reads[circ]['paired'] and read in reads[circ]['mate1'] and read in reads[circ]['mate2']:
-			print('false positive read %s in %s' %(read, circ))
-		    else:
-			unique_reads[circ] += [read]
-	    else:
-		unique_reads[circ] += all_reads
-	return(unique_reads)
+        unique_reads = {}
+        for circ in reads.keys():
+            unique_reads[circ] = []
+            all_reads = list(set(reduce(lambda x, y: x + y, reads[circ].values(), [])))
+            if is_paired:
+                for read in all_reads:
+                    if read in reads[circ]['paired'] and read in reads[circ]['mate1'] and read in reads[circ]['mate2']:
+                        print('false positive read %s in %s' % (read, circ))
+                    else:
+                        unique_reads[circ] += [read]
+            else:
+                unique_reads[circ] += all_reads
+        return (unique_reads)
 
     def write_circles(self, reads, outputfile):
         output_file = open(outputfile, 'w')
@@ -90,12 +90,12 @@ class get_readnames_from_DCC(object):
         junctions = {}
         junctions = self.read_junction_file(self.junction_file, junctions)
         if not self.mate1 == 'none':
-	    junctions = self.read_mate_junction_file(self.mate1, junctions, 'mate1')
+            junctions = self.read_mate_junction_file(self.mate1, junctions, 'mate1')
         if not self.mate2 == 'none':
             junctions = self.read_mate_junction_file(self.mate2, junctions, 'mate2')
         junctions = self.filter_circles_by_circID(circles, junctions)
         if not self.mate1 == 'none' and not self.mate2 == 'none':
-	    unique_reads = self.filter_reads_by_mate(junctions, True)
-	else:
-	    unique_reads = self.filter_reads_by_mate(junctions, False)
+            unique_reads = self.filter_reads_by_mate(junctions, True)
+        else:
+            unique_reads = self.filter_reads_by_mate(junctions, False)
         self.write_circles(unique_reads, '%s.reads.txt' % self.junction_file)
